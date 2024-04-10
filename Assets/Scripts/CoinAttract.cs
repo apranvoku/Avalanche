b90ba9f39af82495716346968537c3e3f94e3fc8
apranvoku <pranvoku.ardit@gmail.com>
2024-04-09 23:34:33 -0400

    Make coin be attracted by player

---
+++ Assets/Scripts/CoinAttract.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class CoinAttract : MonoBehaviour
+{
+    public GameObject player;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.Find("Agent");
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Replace 5f with pickup radius future.
+        if(Vector3.Distance(player.transform.position, agent.transform.position) < 25f)
+        {
+            agent.destination = player.transform.position;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
